3f04024578807a392fd394154af0c04e3b0cb7e9
Jarrod Bradshaw <jarrod.bradshaw@gmail.com>
2018-12-05 09:49:14 +1000

    Add redux and loadable

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,20 @@
 import React from "react";
 import pf from "petfinder-client";
-import { navigate } from "@reach/router";
+import Loadable from "react-loadable";
+import { navigate } from "react-router";
 import Carousel from "./Carousel";
 import Modal from "./Modal";
 
 const petfinder = pf({
   key: process.env.API_KEY,
   secret: process.env.API_SECRET
+});
+
+const loading = () => <h1>loading content...</h1>;
+
+const LoadableContent = Loadable({
+  loader: () => import("./AdoptModalContent"),
+  loading
 });
 
 class Details extends React.Component {
@@ -71,11 +79,7 @@ class Details extends React.Component {
           <p>{description}</p>
           {showModal ? (
             <Modal>
-              <h1>Would you like to adopt {name}?</h1>
-              <div className="buttons">
-                <button onClick={this.toggleModal}>Yes</button>
-                <button onClick={this.toggleModal}>Definitely yes</button>
-              </div>
+              <LoadableContent toggleModal={this.toggleModal} name={name} />
             </Modal>
           ) : null}
         </div>
